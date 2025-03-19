86ac6a1d6b6121a1b61f251714217391fe97b8c6
Bashir Amokomowo <barsheyr@gmail.com>
2025-03-19 22:27:46 +0100

    push

--- frontend/src/app/watches/[slug]/page.tsx
+++ frontend/src/app/watches/[slug]/page.tsx
@@ -1,39 +1,3 @@
-// import GameDetailsClient from "@/components/GameDetails/GameDetailsClient";
-// import GameDetailsServer from "@/components/GameDetails/GameDetailsServer";
-
-// const WatchItem = async (props: { params: { slug: string } }) => {
-//   const {
-//     params: { slug },
-//   } = props;
-
-//   return (
-//     <GameDetailsClient slug={slug}>
-//       <GameDetailsServer slug={slug} />
-//     </GameDetailsClient>
-//   );
-// };
-
-// export default WatchItem;
-
-// "use client";
-
-// import GameDetailsClient from "@/components/GameDetails/GameDetailsClient";
-// import GameDetailsServer from "@/components/GameDetails/GameDetailsServer";
-
-// const GameItem = async (props: { params: { slug: string } }) => {
-//   const {
-//     params: { slug },
-//   } = props;
-
-//   return (
-//     <GameDetailsClient slug={slug}>
-//       <GameDetailsServer slug={slug} />
-//     </GameDetailsClient>
-//   );
-// };
-
-// export default GameItem;
-
 "use client";
 
 import GameDetailsClient from "@/components/GameDetails/GameDetailsClient";
