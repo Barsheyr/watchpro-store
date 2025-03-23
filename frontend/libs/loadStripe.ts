2db827c37adaa03204c3a951975bd87ff9d4ac44
Bashir Amokomowo <barsheyr@gmail.com>
2025-03-24 00:43:23 +0100

    hopefully

--- frontend/libs/loadStripe.ts
+++ frontend/libs/loadStripe.ts
@@ -1,13 +1,24 @@
-import { loadStripe } from "@stripe/stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
 
-let stripePromise: unknown;
+// let stripePromise: unknown;
+
+// export const getStripe = () => {
+//   if (!stripePromise) {
+//     stripePromise = loadStripe(
+//       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+//     );
+//   }
+
+//   return stripePromise;
+// };
+
+import { Stripe, loadStripe } from "@stripe/stripe-js";
+
+let stripePromise: Promise<Stripe | null>;
 
 export const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
-    );
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
   }
-
   return stripePromise;
 };
